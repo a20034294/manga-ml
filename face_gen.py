50a3175be5c766007b38611ef0267cc71ec73604
unknown <a20034294@gmail.com>
2020-01-02 21:03:55 +0800

    init commit

---
+++ face_gen.py
@@ -0,0 +1,58 @@
+import manga109api
+from pprint import pprint
+import cv2
+import time
+import os
+from dotenv import load_dotenv
+
+load_env()
+
+manga109_root_dir = os.getenv('MANGA109_ROOT_DIR')
+p = manga109api.Parser(root_dir=manga109_root_dir)
+
+book_cnt = 0
+os.mkdir('face/')
+
+for book in p.books
+    book_cnt += 1
+    face_cnt = 0
+    os.mkdir('face/' + str(book_cnt))
+    for page in p.annotations[book]["book"]["pages"]["page"]:
+        img = cv2.imread(p.img_path(book=book, index=page["@index"]))
+        if "face" in page.keys() and type(page["face"]) is list:
+            for face in page["face"]:
+                #pprint(face)
+                x, y = face["@xmin"], face["@ymin"]
+                w = face["@xmax"] - x
+                h = face["@ymax"] - y
+                if w > h:
+                    offset = int((w - h) / 2)
+                    face_img = img[y:y + h, x + offset:x + h + offset]
+                else:
+                    offset = int((h - w) / 2)
+                    face_img = img[y + offset:y + w + offset, x:x + w]
+            if face_img.shape[0] >= 50:
+                ok_img = cv2.resize(
+                    face_img, (128, 128), interpolation=cv2.INTER_LANCZOS4)
+                face_cnt += 1
+                cv2.imwrite(
+                    'face/' + str(book_cnt) + '/' + str(face_cnt) + '.jpg',
+                    ok_img)
+        if "face" in page.keys() and type(page["face"]) is dict:
+            face = page["face"]
+            x, y = face["@xmin"], face["@ymin"]
+            w = face["@xmax"] - x
+            h = face["@ymax"] - y
+            if w > h:
+                offset = int((w - h) / 2)
+                face_img = img[y:y + h, x + offset:x + h + offset]
+            else:
+                offset = int((h - w) / 2)
+                face_img = img[y + offset:y + w + offset, x:x + w]
+            if face_img.shape[0] >= 50:
+                ok_img = cv2.resize(
+                    face_img, (128, 128), interpolation=cv2.INTER_LANCZOS4)
+                face_cnt += 1
+                cv2.imwrite(
+                    'face/' + str(book_cnt) + '/' + str(face_cnt) + '.jpg',
+                    ok_img)
---
+++ manga_ml.py
@@ -0,0 +1,92 @@
+# Import Keras libraries and packages
+from tensorflow.keras.models import Sequential  #用來啟動 NN
+from tensorflow.keras.layers import Conv2D  # Convolution Operation
+from tensorflow.keras.layers import MaxPooling2D  # Pooling
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense  # Fully Connected Networks
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.constraints import MaxNorm
+from tensorflow.keras import regularizers
+
+import numpy as np
+import os
+import cv2
+
+# initializing CNN
+model = Sequential()
+model.add(
+    Conv2D(
+        128,
+        5,
+        2,
+        input_shape=(128, 128, 1),
+        activation='relu',
+        padding='same'))
+model.add(Conv2D(128, 5, 2, activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Conv2D(256, 3, 2, activation='relu', padding='same'))
+model.add(Conv2D(256, 3, 2, activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(
+    Dense(
+        units=1024,
+        activation='relu',
+        kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dropout(0.2))
+model.add(
+    Dense(
+        units=1024,
+        activation='relu',
+        kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(units=31, activation='sigmoid'))
+
+model.summary()
+
+x = []
+y = []
+for i in range(1, 31):
+    dirs = os.listdir('face/' + str(i))
+    for file in dirs:
+        x.append(cv2.imread('face/' + str(i) + '/' + file))
+        one_hot = to_categorical(i, 31)
+        y.append(one_hot)
+
+    #break
+
+XX = np.array(x)
+XXX = XX[:, :, :, 0]
+XXX = XXX[:, :, :, np.newaxis]
+print(XXX)
+X = XXX / 255
+
+Y = np.array(y)
+
+from sklearn.utils import shuffle
+X, Y = shuffle(X, Y, random_state=0)
+
+print(X.shape)
+print(Y.shape)
+
+# 定義訓練方式
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['categorical_accuracy'])
+
+# 開始訓練
+train_history = model.fit(
+    x=X,
+    y=Y,
+    validation_split=0.2,
+    epochs=100,
+    batch_size=128,
+    verbose=2,
+    shuffle=True)
+
+model.save('manga_model.h5')
+model.save_weights('manga_model_weights.h5')
