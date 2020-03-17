758565622b91c0e88538904d993d1b620a2798b4
a20034294 <a20034294@gmail.com>
2020-03-18 00:44:00 +0800

    Use data generator to fix ram lacking

---
+++ DataGenerator.py
@@ -0,0 +1,59 @@
+import numpy as np
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.utils import to_categorical
+
+class DataGenerator(Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(128, 128), n_channels=1,
+                 n_classes=91, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+
+        # Generate data
+        X, y = self.__data_generation(indexes)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, indexes):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size, self.n_classes), dtype=int)
+
+
+        # Generate data
+        import cv2
+        for i, idx in enumerate(indexes):
+            # Store sample
+            ID = self.list_IDs[idx]
+            # print(ID)
+            X[i,] = np.array(cv2.imread(ID))[:, :, 0, np.newaxis] / 255
+
+            # Store class
+            y[i] = self.labels[idx]
+
+        return X, y
--- manga_ml.py
+++ manga_ml.py
@@ -12,6 +12,7 @@ from tensorflow.keras import regularizers
 import numpy as np
 import os
 import cv2
+from DataGenerator import DataGenerator
 
 # initializing CNN
 model = Sequential()
@@ -43,36 +44,39 @@ model.add(
         units=1024,
         activation='relu',
         kernel_regularizer=regularizers.l2(0.01)))
-model.add(Dense(units=31, activation='softmax'))
+model.add(Dense(units=91, activation='softmax'))
 
 model.summary()
 
-x = []
-y = []
-for i in range(1, 31):
+list_IDs = []
+labels = []
+
+for i in range(1, 91):
     dirs = os.listdir('face/' + str(i))
     for file in dirs:
-        x.append(cv2.imread('face/' + str(i) + '/' + file))
-        one_hot = to_categorical(i, 31)
-        y.append(one_hot)
+        list_IDs.append('face/' + str(i) + '/' + file)
+        one_hot = to_categorical(i, 91)
+        labels.append(one_hot)
 
-    #break
-
-
-XX = np.array(x)
-
-XXX = XX[:, :, :, 0]
-XXX = XXX[:, :, :, np.newaxis]
-print(XXX)
-X = XXX / 255
-
-Y = np.array(y)
-
+# Shuffle IDs
 from sklearn.utils import shuffle
-X, Y = shuffle(X, Y, random_state=0)
+list_IDs_shu, labels_shu = shuffle(list_IDs, labels, random_state=0)
 
-print(X.shape)
-print(Y.shape)
+from sklearn.model_selection import train_test_split
+seed = 7 # 確保相同的驗證集分割
+IDs_train, IDs_val, labels_train, labels_val = train_test_split(list_IDs_shu, labels_shu, test_size=0.2, random_state=seed)
+
+
+# Generators
+# Parameters
+params = {'dim': (128, 128),
+          'batch_size': 128,
+          'n_classes': 91,
+          'n_channels': 1,
+          'shuffle': True}
+training_generator = DataGenerator(IDs_train, labels_train, **params)
+validation_generator = DataGenerator(IDs_val, labels_val, **params)
+
 
 # 定義訓練方式
 model.compile(
@@ -81,12 +85,12 @@ model.compile(
     metrics=['categorical_accuracy'])
 
 # 開始訓練
-train_history = model.fit(
-    x=X,
-    y=Y,
-    validation_split=0.2,
+train_history = model.fit_generator(
+    generator=training_generator,
+    validation_data=validation_generator,
+    use_multiprocessing=True,
+    workers=1,
     epochs=20,
-    batch_size=128,
     verbose=2,
     shuffle=True)
 
