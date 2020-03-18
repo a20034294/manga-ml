c211a368c52a2a3cc1a96aa4b69be9030c916439
a20034294 <a20034294@gmail.com>
2020-03-18 13:50:49 +0800

    Add bpdy img gen support

--- face_gen.py
+++ face_gen.py
@@ -12,11 +12,14 @@ p = manga109api.Parser(root_dir=manga109_root_dir)
 
 book_cnt = 0
 os.mkdir('face/')
+os.mkdir('body/')
 
 for book in p.books:
     book_cnt += 1
     face_cnt = 0
+    body_cnt = 0
     os.mkdir('face/' + str(book_cnt))
+    os.mkdir('body/' + str(book_cnt))
     for page in p.annotations[book]["book"]["pages"]["page"]:
         img = cv2.imread(p.img_path(book=book, index=page["@index"]))
 
@@ -57,42 +60,31 @@ for book in p.books:
                 cv2.imwrite(
                     'face/' + str(book_cnt) + '/' + str(face_cnt) + '.jpg',
                     ok_img)
-'''
-        if "frame" in page.keys() and type(page["frame"]) is list:
-            for face in page["frame"]:
-                #pprint(face)
-                x, y = face["@xmin"], face["@ymin"]
-                w = face["@xmax"] - x
-                h = face["@ymax"] - y
-                if w > h:
-                    offset = int((w - h) / 2)
-                    face_img = img[y:y + h, x + offset:x + h + offset]
-                else:
-                    offset = int((h - w) / 2)
-                    face_img = img[y + offset:y + w + offset, x:x + w]
-                if face_img.shape[0] >= 50:
+
+        if "body" in page.keys() and type(page["body"]) is list:
+            for body in page["body"]:
+                #pprint(body)
+                x, y = body["@xmin"], body["@ymin"]
+                w = body["@xmax"] - x
+                h = body["@ymax"] - y
+                body_img = img
+                if body_img.shape[0] >= 50 and body_img.shape[1] >= 50:
                     ok_img = cv2.resize(
-                        face_img, (256, 256), interpolation=cv2.INTER_LANCZOS4)
-                    face_cnt += 1
+                        body_img, (128, 128), interpolation=cv2.INTER_LANCZOS4)
+                    body_cnt += 1
                     cv2.imwrite(
-                        'face/' + str(book_cnt) + '/' + str(face_cnt) + '.jpg',
+                        'body/' + str(book_cnt) + '/' + str(body_cnt) + '.jpg',
                         ok_img)
-        if "frame" in page.keys() and type(page["frame"]) is dict:
-            face = page["frame"]
-            x, y = face["@xmin"], face["@ymin"]
-            w = face["@xmax"] - x
-            h = face["@ymax"] - y
-            if w > h:
-                offset = int((w - h) / 2)
-                face_img = img[y:y + h, x + offset:x + h + offset]
-            else:
-                offset = int((h - w) / 2)
-                face_img = img[y + offset:y + w + offset, x:x + w]
-            if face_img.shape[0] >= 50:
+        if "body" in page.keys() and type(page["body"]) is dict:
+            body = page["body"]
+            x, y = body["@xmin"], body["@ymin"]
+            w = body["@xmax"] - x
+            h = body["@ymax"] - y
+            body_img = img
+            if body_img.shape[0] >= 50 and body_img.shape[1] >= 50:
                 ok_img = cv2.resize(
-                    face_img, (256, 256), interpolation=cv2.INTER_LANCZOS4)
-                face_cnt += 1
+                    body_img, (128, 128), interpolation=cv2.INTER_LANCZOS4)
+                body_cnt += 1
                 cv2.imwrite(
-                    'face/' + str(book_cnt) + '/' + str(face_cnt) + '.jpg',
+                    'body/' + str(book_cnt) + '/' + str(body_cnt) + '.jpg',
                     ok_img)
-'''
